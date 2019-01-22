2089d23f1422c37f30336a9a0fa255ab5bc10d4a
luk9400 <lukaszbratos@gmail.com>
2019-01-22 14:31:06 +0100

    added footer and form for adding clients

--- AlkoHurt/controllers/controller.js
+++ AlkoHurt/controllers/controller.js
@@ -1,7 +1,7 @@
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
   host: 'localhost',
-  port: '3307',
+  port: '3306',
   user: 'root',
   password: 'password',
   database: 'alkohurt',
@@ -36,4 +36,13 @@ async function addSupplier(name, nip, street, postal, city, phone, email) {
   return query;
 }
 
-module.exports = {test, addSupplier};
+async function addClient(name, nip, street, postal, city, phone, email) {
+  let conn;
+  conn = await pool.getConnection();
+  const query = 'INSERT INTO client (name, nip, street_and_number, postal_code, city, phone_number, email) VALUES (?, ?, ?, ?, ?, ?, ?)';
+  await conn.query(query, [name, nip, street, postal, city, phone, email]);
+  conn.end();
+  return query;
+}
+
+module.exports = {test, addSupplier, addClient};
--- AlkoHurt/routes/index.js
+++ AlkoHurt/routes/index.js
@@ -7,13 +7,26 @@ router.get('/', function(req, res, next) {
   res.render('indexAdmin', { title: 'AlkoHurt' });
 });
 
-router.get('/supplier', function (req, res, next) {
-  res.render('supplier');
+router.get('/add-supplier', function (req, res, next) {
+  res.render('add-supplier');
 });
 
 router.post('/add_supplier', function (req, res, next) {
   try {
     controller.addSupplier(req.body.name, req.body.nip, req.body.street, req.body.postal, req.body.city, req.body.phone, req.body.email);
+    res.redirect('/');
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.get('/add-client', function (req, res, next) {
+  res.render('add-client');
+});
+
+router.get('/add_client', function (req, res, next) {
+  try {
+    controller.addClient(req.body.name, req.body.nip, req.body.street, req.body.postal, req.body.city, req.body.phone, req.body.email);
     res.redirect('/');
   } catch (e) {
     console.log(e);
