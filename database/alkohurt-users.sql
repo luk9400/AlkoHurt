0028e4e692c2dd68817c162f6b8ee33aeb191b20
luk9400 <lukaszbratos@gmail.com>
2019-01-26 18:09:49 +0100

    fixes

--- database/alkohurt-users.sql
+++ database/alkohurt-users.sql
@@ -1,3 +1,12 @@
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'password';
-CREATE USER 'manager'@'localhost' IDENTIFIED BY 'password';
-CREATE USER 'worker'@'localhsot' IDENTIFIED BY 'password';
+USE alkohurt;
+
+CREATE USER 'app'@'%' IDENTIFIED BY 'apppassword';
+CREATE USER 'admin'@'%' IDENTIFIED BY 'adminpassword';
+CREATE USER 'manager'@'%' IDENTIFIED BY 'managerpassword';
+CREATE USER 'worker'@'%' IDENTIFIED BY 'workerpassword';
+
+GRANT ALL PRIVILEGES ON 'alkohurt'.'users' TO 'app'@'%';
+
+GRANT SELECT, INSERT, DELETE, UPDATE ON 'alkohurt'.* TO 'admin'@'%';
+
+FLUSH PRIVILEGES;
