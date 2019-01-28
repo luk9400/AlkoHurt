c5dc07a7dbded9246e38dd707d7e46fd3d0be94a
luk9400 <lukaszbratos@gmail.com>
2019-01-28 17:57:39 +0100

    route fix

--- AlkoHurt/routes/index.js
+++ AlkoHurt/routes/index.js
@@ -71,149 +71,224 @@ router.post('/add_client', function (req, res) {
 });
 
 router.get('/add-wine', function (req, res) {
-  res.render('add-wine', {
-    nick: req.session.login
-  });
+  if (req.session.type === 'manager' || req.session.type === 'admin') {
+    res.render('add-wine', {
+      nick: req.session.login
+    });
+  } else if (req.session.login) {
+    res.redirect('/');
+  } else {
+    res.redirect('/login');
+  }
 });
 
 router.post('/add_wine', async function (req, res) {
-  try {
-    await controller.addWine(req.session.type, req.body.name, req.body.color, req.body.abv, req.body.type, req.body.capacity,
-      req.body.country_of_origin, req.body.price);
-    res.redirect('/')
-  } catch (e) {
-    console.log(e);
+  if (req.session.type === 'manager' || req.session.type === 'admin') {
+    try {
+      await controller.addWine(req.session.type, req.body.name, req.body.color, req.body.abv, req.body.type, req.body.capacity,
+        req.body.country_of_origin, req.body.price);
+      res.redirect('/')
+    } catch (e) {
+      console.log(e);
+    }
+  } else if (req.session.login) {
+    res.redirect('/');
+  } else {
+    res.redirect('/login');
   }
 });
 
 router.get('/add-beer', function (req, res) {
-  res.render('add-beer', {
-    nick: req.session.login
-  });
+  if (req.session.type === 'manager' || req.session.type === 'admin') {
+    res.render('add-beer', {
+      nick: req.session.login
+    });
+  } else if (req.session.login) {
+    res.redirect('/');
+  } else {
+    res.redirect('/login');
+  }
 });
 
 router.post('/add_beer', async function (req, res) {
-  try {
-    await controller.addBeer(req.session.type, req.body.name, req.body.brew, req.body.abv, req.body.type, req.body.capacity,
-      req.body.container_type, req.body.price);
-    res.redirect('/')
-  } catch (e) {
-    console.log(e);
+  if (req.session.type === 'manager' || req.session.type === 'admin') {
+    try {
+      await controller.addBeer(req.session.type, req.body.name, req.body.brew, req.body.abv, req.body.type, req.body.capacity,
+        req.body.container_type, req.body.price);
+      res.redirect('/')
+    } catch (e) {
+      console.log(e);
+    }
+  } else if (req.session.login) {
+    res.redirect('/');
+  } else {
+    res.redirect('/login');
   }
 });
 
 router.get('/add-liquor', function (req, res) {
-  res.render('add-liquor', {
-    nick: req.session.login
-  });
+  if (req.session.type === 'manager' || req.session.type === 'admin') {
+    res.render('add-liquor', {
+      nick: req.session.login
+    });
+  } else if (req.session.login) {
+    res.redirect('/');
+  } else {
+    res.redirect('/login');
+  }
 });
 
 router.post('/add_liquor', async function (req, res) {
-  try {
-    await controller.addLiquor(req.session.type, req.body.name, req.body.type, req.body.abv, req.body.capacity, req.body.price);
-    res.redirect('/')
-  } catch (e) {
-    console.log(e);
+  if (req.session.type === 'manager' || req.session.type === 'admin') {
+    try {
+      await controller.addLiquor(req.session.type, req.body.name, req.body.type, req.body.abv, req.body.capacity, req.body.price);
+      res.redirect('/')
+    } catch (e) {
+      console.log(e);
+    }
+  } else if (req.session.login) {
+    res.redirect('/');
+  } else {
+    res.redirect('/login');
   }
 });
 
 router.get('/plan-sale', async function (req, res) {
-  controller.getPlanSaleData(req.session.type)
-    .then(e => {
-      res.render('plan-sale', {data: e});
-    }).catch(e => {
-    console.log(e);
-  })
+  if (req.session.login) {
+    controller.getPlanSaleData(req.session.type)
+      .then(e => {
+        res.render('plan-sale', {data: e});
+      }).catch(e => {
+      console.log(e);
+    })
+  } else {
+    res.redirect('/login');
+  }
 });
 
 router.post('/plan_sale', async function (req, res) {
-  await controller.planSale(req.session.type, req.body)
-    .then(() => {
-      console.log("Sale planned");
-      res.redirect('/');
-    })
-    .catch(e => {
-      console.log(e);
-      res.redirect('/');
-    });
+  if (req.session.login) {
+    await controller.planSale(req.session.type, req.body)
+      .then(() => {
+        console.log("Sale planned");
+        res.redirect('/');
+      })
+      .catch(e => {
+        console.log(e);
+        res.redirect('/');
+      });
+  } else {
+    res.redirect('/login');
+  }
 });
 
 router.get('/update-sale', async function (req, res) {
-  controller.getSales(req.session.type)
-    .then(e => {
-      console.log(e);
-      res.render('update-sale', {
-        data: e,
-        nick: req.session.login
+  if (req.session.login) {
+    controller.getSales(req.session.type)
+      .then(e => {
+        console.log(e);
+        res.render('update-sale', {
+          data: e,
+          nick: req.session.login
+        });
       });
-    });
+  } else {
+    res.redirect('/login');
+  }
 });
 
 router.post('/update_sale', async function (req, res) {
-  controller.updateSale(req.session.type, req.body.saleSelect).then(() => {
-    res.redirect('/');
-  });
+  if (req.session.login) {
+    controller.updateSale(req.session.type, req.body.saleSelect).then(() => {
+      res.redirect('/');
+    });
+  } else {
+    res.redirect('/login');
+  }
 });
 
 router.get('/plan-supply', async function (req, res) {
-  controller.getNames(req.session.type)
-    .then(e => {
-      // console.log(e);
-      res.render('plan-supply', {
-        data: e,
-        nick: req.session.login
-      });
-    }).catch(e => {
-    console.log(e);
-    res.redirect('/');
-  });
+  if (req.session.login) {
+    controller.getNames(req.session.type)
+      .then(e => {
+        // console.log(e);
+        res.render('plan-supply', {
+          data: e,
+          nick: req.session.login
+        });
+      }).catch(e => {
+      console.log(e);
+      res.redirect('/');
+    });
+  } else {
+    res.redirect('/login');
+  }
 });
 
 router.post('/plan_supply', async function (req, res) {
-  // console.log(req.body);
-  await controller.planSupply(req.session.type, req.body)
-    .then(() => {
-      console.log("Supply planned");
-      res.redirect('/');
-    })
-    .catch(e => {
-      console.log(e);
-      res.redirect('/');
-    });
+  if (req.session.login) {
+    await controller.planSupply(req.session.type, req.body)
+      .then(() => {
+        console.log("Supply planned");
+        res.redirect('/');
+      })
+      .catch(e => {
+        console.log(e);
+        res.redirect('/');
+      });
+  } else {
+    res.redirect('/login');
+  }
 });
 
 router.get('/update-supply', async function (req, res) {
-  controller.getSupplies(req.session.type)
-    .then(e => {
-      console.log(e);
-      res.render('update-supply', {
-        data: e,
-        nick: req.session.login
+  if (req.session.login) {
+    controller.getSupplies(req.session.type)
+      .then(e => {
+        console.log(e);
+        res.render('update-supply', {
+          data: e,
+          nick: req.session.login
+        });
       });
-    });
+  } else {
+    res.redirect('/login');
+  }
 });
 
 router.post('/update_supply', async function (req, res) {
-  controller.updateSupply(req.session.type, req.body.supplySelect).then(() => {
-    res.redirect('/');
-  });
+  if (req.session.login) {
+    controller.updateSupply(req.session.type, req.body.supplySelect).then(() => {
+      res.redirect('/');
+    });
+  } else {
+    res.redirect('/login');
+  }
 });
 
 router.get('/show-quantity', async function (req, res) {
-  controller.getProducts(req.session.type)
-    .then(e => {
-      res.render('show-quantity', {
-        data: e,
-        nick: req.session.login
+  if (req.session.login) {
+    controller.getProducts(req.session.type)
+      .then(e => {
+        res.render('show-quantity', {
+          data: e,
+          nick: req.session.login
+        });
       });
-    })
+  } else {
+    res.redirect('/login');
+  }
 });
 
 router.post('/show_quantity', async function (req, res) {
-  controller.quantityOnDate(req.session.type, req.body.product_id, req.body.date)
-    .then(e => {
-      res.json({quantity: e});
-    });
+  if (req.session.login) {
+    controller.quantityOnDate(req.session.type, req.body.product_id, req.body.date)
+      .then(e => {
+        res.json({quantity: e});
+      });
+  } else {
+    res.redirect('/login');
+  }
 });
 
 router.get('/login', function (req, res) {
