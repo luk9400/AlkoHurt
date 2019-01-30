5e7bba18752c81c6ea156ed25b75b77f51d66134
luk9400 <lukaszbratos@gmail.com>
2019-01-30 01:42:09 +0100

    delete user fix

--- AlkoHurt/public/javascripts/delete-user.js
+++ AlkoHurt/public/javascripts/delete-user.js
@@ -6,6 +6,9 @@ function fillUserSelect() {
   let userSelect = document.getElementById('userSelect');
 
   for (let user of users) {
+    if (user.login === login) {
+      continue;
+    }
     let option = document.createElement('option');
     option.appendChild(document.createTextNode(user.login));
     userSelect.appendChild(option);
